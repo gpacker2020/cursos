a98455e1bc43a6db321666e6f837df7332d854bd
Germano <germanopacker@gmail.com>
2020-10-24 16:25:09 -0300

    testando apenas

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ import './config/msg'
 import store from './config/store'
 import router from './config/router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false  
 
 //TEMPORARIA
 require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikdlcm1hbm8gIiwiZW1haWwiOiJnZXJtYW5vQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2MDM1NjQwMTEsImV4cCI6MTYwMzgyMzIxMX0.9FLco3lcd1LYGshWocIAg4f-xyQhB8EEb3UpKOWfylc'
